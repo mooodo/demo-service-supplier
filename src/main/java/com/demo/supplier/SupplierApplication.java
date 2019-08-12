dfb6aa57556da77cf60fbbeba510c8ce7a4e44e7
mooodo <fangang2004@126.com>
2019-08-12 17:36:52 +0800

    commit all

--- src/main/java/com/demo/supplier/SupplierApplication.java
+++ src/main/java/com/demo/supplier/SupplierApplication.java
@@ -6,6 +6,7 @@ package com.demo.supplier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @EnableEurekaClient
 @Configuration
+@ComponentScan(basePackages="com.demo")
 @ImportResource(locations={"classpath:applicationContext.xml"})
 public class SupplierApplication {
 
